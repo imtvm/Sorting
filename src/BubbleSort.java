323503dcc58c3ff010cd95346a51ff1e03d2402b
imtvm <t.vergismayo@gmail.com>
2018-11-07 22:42:05 -0500

    Completed BubbleSort and added Tester method

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,2 +1,18 @@
 public class BubbleSort {
+
+    public int[] bubbleSort(int[] a){
+
+        for (int i = 0; i < a.length-1; i++){
+            for (int j = 0; j < a.length-i-1; j++){
+                if (a[j] > a[j+1]){
+                    int tempInt = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = tempInt;
+                }
+            }
+        }
+
+        return a;
+    }
+
 }
---
+++ src/SortingTester.java
@@ -0,0 +1,18 @@
+public class SortingTester {
+    public static void main (String[] args){
+        BubbleSort bs = new BubbleSort();
+        MergeSort ms = new MergeSort();
+        int[] testArray = {54,23,67,1,45,176,2,32,21};
+
+        printArray("BubbleSort",bs.bubbleSort(testArray));
+        printArray("MergeSort", ms.mergeSort(testArray));
+    }
+
+    public static void printArray(String type, int[] a){
+        System.out.print(type + ": ");
+        for (int i =0; i < a.length; i++){
+            System.out.print(a[i] + " ");
+        }
+        System.out.println("");
+    }
+}
