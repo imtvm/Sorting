460de8dd4f7ead39ac8a40dfd66977896f5609c5
tvm <t.vergismayo@gmail.com>
2018-11-08 23:51:51 -0500

    Update SortingTester.java

--- src/SortingTester.java
+++ src/SortingTester.java
@@ -2,12 +2,12 @@ public class SortingTester {
     public static void main (String[] args){
         BubbleSort bs = new BubbleSort();
         MergeSort ms = new MergeSort();
-        StalinSort ss = new StalinSort();
+        
         int[] testArray = {54,23,67,1,45,176,2,32,21};
 
         printArray("BubbleSort",bs.bubbleSort(testArray));
         printArray("MergeSort", ms.mergeSort(testArray));
-        printArray("StalinSort", ss.stalinSort(testArray));
+        
     }
 
     public static void printArray(String type, int[] a){
